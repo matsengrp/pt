f87ca71805805b661efaca28f1481aec3737da57
Erick Matsen <matsen@fredhutch.org>
2016-06-07 16:51:20 -0700

    Adding test data.

--- src/test.cpp
+++ src/test.cpp
@@ -12,11 +12,11 @@ namespace pt {
 TEST_CASE("Partition", "[partition]") {
 
  auto p = std::unique_ptr<Partition>(
-   new Partition("test-data/five/five.tre", "test-data/five/five.fasta"));
+   new Partition("test-data/five/RAxML_bestTree.five", "test-data/five/five.fasta"));
 
  std::cout << p->ToNewick() << std::endl;
  REQUIRE(p->branch_count() == 7);
- REQUIRE(-738.179793 - p->FullTraversalLogLikelihood() < 1e-6);
+ REQUIRE(-4159.020246 - p->FullTraversalLogLikelihood() < 1e-6);
 
 }
 
