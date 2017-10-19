701a414d248d2a3c74b659fc1ec256b574165cb5
Brian Claywell <bclaywel@fhcrc.org>
2017-10-19 14:11:55 -0700

    [WIP] Make position class a thinner wrapper.

--- src/position.cpp
+++ src/position.cpp
@@ -6,14 +6,9 @@
 
 namespace pt {
 
-Position::Position(pll_utree_t* tree, pll::Model model) :
+Position::Position(pll_utree_t* tree, const pll::Model& model) :
     tree_(tree),
     model_(model)
 { }
-
-Position::~Position()
-{
-  pll_utree_destroy(tree_, pll::cb_erase_data);
-}
 
 } // namespace pt
--- src/position.hpp
+++ src/position.hpp
@@ -12,9 +12,7 @@ class Position {
   pll::Model model_;
 
  public:
-  Position(pll_utree_t* tree, pll::Model model);
-
-  ~Position();
+  Position(pll_utree_t* tree, const pll::Model& model);
 
   pll_utree_t* GetTree() const;
   pll::Model GetModel() const;
