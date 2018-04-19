5026c8b8e219e2aee66cbff1dcff12ee1e4864c7
Brian Claywell <bclaywel@fhcrc.org>
2018-04-19 13:53:02 -0700

    Fix unit test.

--- test/test_ptw.cpp
+++ test/test_ptw.cpp
@@ -510,9 +510,9 @@ TEST_CASE("guru operations on DS1 are correct", "[guru_DS1]") {
     options.marginal_mode = true;
     options.move_tester = std::make_shared<pt::move_tester::SingleBranchOptimizer>(options);
 
-    size_t good_tree_count = 80;
-    size_t visited_tree_count = 264;
-    size_t tested_tree_count = 3427;
+    size_t good_tree_count = 61;
+    size_t visited_tree_count = 270;
+    size_t tested_tree_count = 2659;
 
     RunGuruTest(options, tree, model, labels, sequences,
                 good_tree_count, visited_tree_count, tested_tree_count);
